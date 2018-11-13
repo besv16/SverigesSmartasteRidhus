1dc11ca35aaa4f138c58bb81da6bf20105d5d281
besv16 <beasve0506@skola.goteborg.se>
2018-11-13 15:57:52 +0100

    tiny changes

---
+++ src/components/Timespan/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import Chart from '../Chart'
+import './index.css'
+
+class Card extends Component {
+
+  componentWillMount() {
+    //this.initSocket()
+  }
+
+  render() {
+
+    return (
+      <div className={this.props.type} style={{ gridColumnStart: this.props.colStart, gridColumnEnd: this.props.colEnd, gridRowStart: this.props.rowStart, gridRowEnd: this.props.rowEnd }}>
+        <div className="headline">
+          <p>{this.props.title}</p>
+          <div className="open">
+            <img src="/icons/fullscreen_icon.svg" alt="ikon för fullskärm" />
+          </div>
+        </div>
+        <Chart endPoint={this.props.endPoint} />
+      </div>
+    )
+  }
+
+}
+
+export default Card
