5a72ecc8d2d0d69c73dade91b20ecb09484db6b8
besv16 <beasve0506@skola.goteborg.se>
2018-11-18 19:20:01 +0100

    Single Card view in progress

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,23 +4,49 @@ import './index.css'
 
 class Card extends Component {
 
-  componentWillMount() {
-    //this.initSocket()
+  state = {
+    fullScreenBool: false
+  }
+
+  fullScreen(e) {
+      this.setState({
+          fullScreenBool: e
+      })
   }
 
   render() {
 
-    return (
-      <div className={this.props.type} style={{ gridColumnStart: this.props.colStart, gridColumnEnd: this.props.colEnd, gridRowStart: this.props.rowStart, gridRowEnd: this.props.rowEnd }}>
-        <div className="headline">
-          <p>{this.props.title}</p>
-          <div className="open">
-            <img src="/icons/fullscreen_icon.svg" alt="ikon för fullskärm" />
+    // HÄR KOLLAR VI OM IKONEN FÖR HELSKÄRMSVYN
+    //BLIVIT KLICKAD PÅ ELLER EJ OCH RENDERAR OLIKA
+    //VYER BEROENDE PÅ DETTA
+    if (this.state.fullScreenBool === false) {
+      return (
+        <div className={this.props.type} style={{ gridColumnStart: this.props.colStart, gridColumnEnd: this.props.colEnd, gridRowStart: this.props.rowStart, gridRowEnd: this.props.rowEnd }}>
+          <div className="headline">
+            <p>{this.props.title}</p>
+            <div className="open">
+              <img src="/icons/fullscreen_icon.svg" alt="ikon för fullskärm" onClick={(e) => this.fullScreen(true)} />
+            </div>
           </div>
+          <Chart endPoint={this.props.endPoint} />
         </div>
-        <Chart endPoint={this.props.endPoint} />
-      </div>
-    )
+      )
+    }
+
+    if (this.state.fullScreenBool === true) {
+      return (
+        <div className="fullScreen">
+          <div className="headline">
+            <p>{this.props.title}</p>
+          </div>
+          <div className="close" onClick={(e) => this.fullScreen(false)}>
+            <p>Stäng</p>
+          </div>
+          <Chart endPoint={this.props.endPoint} />
+        </div>
+      )
+    }
+
   }
 
 }
