2581b5039af9c29efc8e641ddb3eeb68f6a32d2c
besv16 <beasve0506@skola.goteborg.se>
2018-11-20 19:08:46 +0100

    Updates

---
+++ precache-manifest.1c61e623844cdc42c0e3e526e3ed79ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2aa8a44e9247d5c40594",
+    "url": "/besv16/SverigesSmartasteRidhus/static/js/runtime~main.2aa8a44e.js"
+  },
+  {
+    "revision": "8a256e6eb5f72e92d1cb",
+    "url": "/besv16/SverigesSmartasteRidhus/static/js/main.8a256e6e.chunk.js"
+  },
+  {
+    "revision": "582afbec836b9026d556",
+    "url": "/besv16/SverigesSmartasteRidhus/static/js/1.582afbec.chunk.js"
+  },
+  {
+    "revision": "8a256e6eb5f72e92d1cb",
+    "url": "/besv16/SverigesSmartasteRidhus/static/css/main.396a8b58.chunk.css"
+  },
+  {
+    "revision": "582afbec836b9026d556",
+    "url": "/besv16/SverigesSmartasteRidhus/static/css/1.8f35729c.chunk.css"
+  },
+  {
+    "revision": "6bf973a9879561ebcfe226b4b4dd0f94",
+    "url": "/besv16/SverigesSmartasteRidhus/index.html"
+  }
+];
